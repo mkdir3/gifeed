9fe582c09efd47e2e1877e71a58e7d4c0bd3bd51
Ted <p0599847@protonmail.com>
2022-05-27 17:42:54 +0200

    final

--- js/gifeed.js
+++ js/gifeed.js
@@ -1,5 +1,24 @@
 const API_KEY = `Te9zomeC68QwNNPccwsvP7L3yDaTIv73`;
 
+// Elements needed
+const searchEl = document.querySelector(".search-input");
+const hintEl = document.querySelector(".search-hint");
+const videoEl = document.querySelector(".videos");
+const clearEl = document.querySelector(".search-clear");
+
+// Display loading element
+const toggleLoading = (state) => {
+  if (state) {
+    document.body.classList.add("loading");
+    searchEl.disabled = true;
+  } else {
+    document.body.classList.remove("loading");
+    searchEl.disabled = false;
+    searchEl.focus();
+  }
+};
+
+// Video element
 function createVideo(src) {
   const video = document.createElement("video");
   video.className = "video";
@@ -13,7 +32,14 @@ function createVideo(src) {
   return video;
 }
 
-const fetchGiffy = (searchTerm) => {
+// Random length for fetch differents gifs
+function getRandomInt(length) {
+  return Math.floor(Math.random() * length);
+}
+
+// Fetch function
+const fetchGiphy = (searchTerm) => {
+  toggleLoading(true);
   fetch(
     `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${searchTerm}&limit=25&offset=0&rating=r&lang=fr`
   )
@@ -21,21 +47,26 @@ const fetchGiffy = (searchTerm) => {
       return response.json();
     })
     .then((json) => {
-      const content = json.data[0];
+      const length = json.data.length;
+      const content = json.data[getRandomInt(length)];
       const src = content.images.original.mp4;
 
       const video = createVideo(src);
-      const videoEl = document.querySelector(".videos");
       videoEl.appendChild(video);
+
+      video.addEventListener("loadeddata", (e) => {
+        video.classList.add("visible");
+        toggleLoading(false);
+        document.body.classList.add("has-results");
+        hintEl.innerHTML = `Pressez entrez pour voir d'autres ${searchTerm}`;
+      });
     })
     .catch((error) => {
-      console.log(error);
+      hintEl.innerHTML = `Il y a un probleme`;
     });
 };
 
-const searchEl = document.querySelector(".search-input");
-const hintEl = document.querySelector(".search-hint");
-
+// Events functions
 const doSearch = (e) => {
   const searchTerm = searchEl.value;
 
@@ -47,8 +78,23 @@ const doSearch = (e) => {
   }
 
   if (e.key === "Enter" && searchTerm.length > 2) {
-    fetchGiffy(searchTerm);
+    fetchGiphy(searchTerm);
   }
 };
+const clearSearch = () => {
+  document.body.classList.remove("has-results");
+  searchEl.innerHTML = "";
+  videoEl.innerHTML = "";
+  hintEl.innerHTML = "";
 
+  searchEl.focus();
+};
+document.addEventListener("keyup", (e) => {
+  if (e.key === "Escape") {
+    clearSearch();
+  }
+});
+
+// Events
 searchEl.addEventListener("keyup", doSearch);
+clearEl.addEventListener("click", clearSearch);
